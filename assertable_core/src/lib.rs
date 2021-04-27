cdf082a17850371aa07b0a30fd84cf92e9de515a
Alexandra Østermark <alex.cramt@gmail.com>
2021-04-27 19:56:16 +0200

    first commit

---
+++ assertable_core/src/lib.rs
@@ -0,0 +1,112 @@
+use once_cell::sync::Lazy;
+use std::borrow::Cow;
+use std::fmt::Debug;
+use std::ops::Deref;
+
+pub trait Assertable: Debug {
+    fn test_eq(&self, other: &Self) -> bool;
+    fn assert_eq(&self, other: &Self) {
+        assert!(
+            Self::test_eq(self, other),
+            "a = {:?}, b = {:?}",
+            self,
+            other
+        )
+    }
+}
+
+macro_rules! eq_assertable_impl {
+    ($x:ty) => {
+        impl Assertable for $x {
+            fn test_eq(&self, other: &Self) -> bool{
+                self.eq(other)
+            }
+        }
+    };
+}
+
+static ASSERTABLE_FLOAT_DIFF_MODE: Lazy<f64> = Lazy::new(|| {
+    option_env!("ASSERTABLE_FLOAT_DIFF_MODE")
+        .map(|x| x.parse().unwrap())
+        .unwrap_or(0.0001)
+});
+
+macro_rules! float_assertable_impl {
+    ($x:ty) => {
+        impl Assertable for $x {
+            fn test_eq(&self, other: &Self) -> bool{
+                (self - other).abs() < (*ASSERTABLE_FLOAT_DIFF_MODE.deref() as $x)
+            }
+        }
+    };
+}
+
+eq_assertable_impl!(String);
+eq_assertable_impl!(&str);
+eq_assertable_impl!(bool);
+eq_assertable_impl!(u8);
+eq_assertable_impl!(i8);
+eq_assertable_impl!(u16);
+eq_assertable_impl!(i16);
+eq_assertable_impl!(u32);
+eq_assertable_impl!(i32);
+eq_assertable_impl!(u64);
+eq_assertable_impl!(i64);
+eq_assertable_impl!(u128);
+eq_assertable_impl!(i128);
+eq_assertable_impl!(usize);
+eq_assertable_impl!(isize);
+
+float_assertable_impl!(f32);
+float_assertable_impl!(f64);
+
+impl<K> Assertable for Option<K>
+    where
+        K: Assertable,
+        K: Debug,
+{
+    fn test_eq(&self, other: &Self) -> bool{
+        match (self, other) {
+            (None, None) => true,
+            (Some(a), Some(b)) => a.test_eq(b),
+            _ => false,
+        }
+    }
+}
+
+impl<L, K> Assertable for Result<L, K>
+    where
+        L: Assertable,
+        L: Debug,
+        K: Assertable,
+        K: Debug,
+{
+    fn test_eq(&self, other: &Self) -> bool{
+        match (self, other) {
+            (Err(a), Err(b)) => a.test_eq(b),
+            (Ok(a), Ok(b)) => a.test_eq(b),
+            _ => false,
+        }
+    }
+}
+
+impl<K> Assertable for Cow<'_, K>
+    where
+        K: Assertable,
+        K: Debug,
+        K: Clone,
+{
+    fn test_eq(&self, other: &Self) -> bool{
+        self.deref().test_eq(other.deref())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Assertable;
+
+    #[test]
+    fn test() {
+        "a".assert_eq(&"a")
+    }
+}
